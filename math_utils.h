2b6146d23dfffef04327bed7bae60e0d92e672a4
Laksh <lakshtaneja@yahoo.com>
2025-11-03 18:57:58 +0530

    Refactor math specific types from utils to math_utils, add Equation, Element, Function types

--- math_utils.h
+++ math_utils.h
@@ -2,6 +2,40 @@
 
 #define math_utils
 
-struct function parse_func(char* input);
+
+typedef struct {
+     char condition[50];
+     char output[50];
+} Cond;
+typedef struct {
+     char variable[10];
+     Cond conds[100];
+} Function;
+
+typedef enum{
+    LITERAL,
+    VARIABLE,
+    LESS_THAN,
+    GREATER_THAN,
+    EQUALS_TO,
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+} ElementType;
+
+typedef struct {
+    ElementType type;
+    int elementValue;
+} Element;
+
+typedef struct {
+    int capacity;
+    int count;
+    Element* elements;
+} Equation;
+
+Function parse_func(char* input);
+Equation parse_eq(char* input);
 
 #endif
--- utils.h
+++ utils.h
@@ -1,18 +1,6 @@
 #ifndef utils
 #define utils
 
-//vars
-
-struct conds{
-     char condition[50];
-     char output[50];
-};
-
-struct function{
-     char variable[10];
-     struct conds conds[100];
-};
-
 //functions
 void delims(char* output, int varsize, char** input, char* delim);
 
