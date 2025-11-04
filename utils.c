e24495bbe92c170774304b1f15bae30fce3c8a15
Laksh <lakshtaneja@yahoo.com>
2025-11-04 19:58:29 +0530

    Add Stack structure & methods

--- utils.c
+++ utils.c
@@ -1,6 +1,7 @@
 #include "utils.h"
 #include <ctype.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 
@@ -19,4 +20,36 @@ void delims(char* output, int varsize, char** input, char* delim){
     }
     *(input) += 1;
     delims(output, varsize, input, delim);
+}
+
+void stack_init(Stack *s, int element_size){
+    s->elementSize = element_size;
+    s->capacity = 32;
+    s->data = malloc(s->capacity * s->elementSize);
+    s->top = -1;
+}
+
+void stack_push(Stack* s, const void* data){
+    if (s->top+1 >=s->capacity){
+        s->capacity *= 2;
+        s->data = realloc(s->data, s->capacity * s->elementSize);
+    }
+    s->top++;
+    void* target = (char*)s->data + s->top * s->elementSize; // char* moves 1 byte, void* cant do arithmetic
+    memcpy(target, data, s->elementSize);
+}
+
+void stack_pop(Stack *s, void* out_value){
+    if (s->top < 0) return;
+    void* source = (char*)s->data + s->top * s->elementSize;
+    if (out_value){
+        memcpy(out_value, source, s->elementSize);
+    }
+    s->top--;
+}
+
+void stack_free(Stack *s){
+    free(s->data);
+    s->top = -1;
+    s->capacity = 0;
 }
--- utils.h
+++ utils.h
@@ -1,7 +1,18 @@
 #ifndef utils
 #define utils
 
+typedef struct {
+    void* data;
+    int elementSize;
+    int capacity;
+    int top;
+} Stack;
+
 //functions
 void delims(char* output, int varsize, char** input, char* delim);
+void stack_init(Stack* s, int element_size);
+void stack_push(Stack* s, const void* value);
+void stack_pop(Stack* s, void* out_value);
+void stack_free(Stack* s);
 
 #endif
